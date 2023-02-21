36d388d5f86cb7a4b1471245e35953791d69f638
shabandinho <mahmoudshaband@gmail.com>
2023-02-22 00:20:38 +0200

    wip

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,15 +9,35 @@ import (
 
 func main() {
 	router := gin.Default()
-	files := template.Must(template.New("").ParseFS(templates.FS, "*.gohtml", "site/layout/*.gohtml", "site/pages/*.gohtml"))
+	files := template.Must(template.New("").ParseFS(templates.FS, "site/layout/*.gohtml", "site/pages/*.gohtml", "*.gohtml"))
 	router.SetHTMLTemplate(files)
 	router.Static("assets", "./assets")
 	router.GET("/", func(context *gin.Context) {
-
-		context.HTML(http.StatusOK, "index.gohtml", gin.H{
-			"title": "Posts",
-		})
+		context.HTML(http.StatusOK, "index.gohtml", gin.H{})
 	})
+	//router.GET("/about", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/about.gohtml", gin.H{})
+	//})
+	//
+	//router.GET("/category", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/category.gohtml", gin.H{})
+	//})
+	//
+	//router.GET("/contact", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/contact.gohtml", gin.H{})
+	//})
+	//
+	//router.GET("/gallery", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/gallery.gohtml", gin.H{})
+	//})
+	//
+	//router.GET("/services", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/services.gohtml", gin.H{})
+	//})
+	//
+	//router.GET("/pill", func(context *gin.Context) {
+	//	context.HTML(http.StatusOK, "site/pages/pill.gohtml", gin.H{})
+	//})
 
 	err := router.Run(":8080")
 	if err != nil {
