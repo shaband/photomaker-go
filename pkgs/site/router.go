cccc7ca7843ada5382ece976e6fb4486139b784e
shaband <mahmoudshaband@gmail.com>
2023-07-09 00:57:13 +0300

    add code of login

--- pkgs/site/router.go
+++ pkgs/site/router.go
@@ -9,6 +9,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/shaband/photomaker-go/pkgs/infrastucture/database"
 	"github.com/shaband/photomaker-go/pkgs/infrastucture/middleware"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 )
 
 func SiteRegister(router *gin.RouterGroup) {
@@ -33,6 +35,52 @@ func SiteRegister(router *gin.RouterGroup) {
 
 	router.GET("/admin/login", func(ctx *gin.Context) {
 		ctx.HTML(http.StatusOK, "admin.auth.login.gohtml", gin.H{})
+	})
+
+	authMiddleware := func() gin.HandlerFunc {
+		return func(c *gin.Context) {
+			session := sessions.Default(c)
+			user := session.Get("user")
+
+			if user == nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "Not authenticated"})
+				c.Abort()
+				return
+			}
+
+			c.Next()
+		}
+	}
+
+	// Protected route that requires authentication
+	router.GET("/protected", authMiddleware(), func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
+		c.JSON(http.StatusOK, gin.H{"message": "Authenticated", "user": user})
+	})
+
+	// Login route
+	router.POST("admin/login", func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := c.PostForm("user")
+
+		// Perform authentication logic, e.g., check credentials against a database
+
+		// Simulating a successful authentication
+		session.Set("user", user)
+		session.Save()
+
+		c.JSON(http.StatusOK, gin.H{"message": "Logged in", "user": user})
+	})
+
+	// Logout route
+	router.POST("/logout", func(c *gin.Context) {
+		session := sessions.Default(c)
+		
+		session.Clear()
+		session.Save()
+
+		c.JSON(http.StatusOK, gin.H{"message": "Logged out"})
 	})
 }
 
